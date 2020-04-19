c3c63c179d1f076ea8e44fe5ab5cbe8396ddfb94
Gustavo Millen <millengustavo@gmail.com>
2020-04-19 15:14:57 -0300

    fixed zoom on peak date

--- src/plot_simulation.py
+++ src/plot_simulation.py
@@ -6,7 +6,7 @@ import streamlit as st
 # plt.style.use("seaborn-whitegrid")
 
 
-def plot_simulation_output(df_simulated_data, zoom_on=None, zoom_length=60):
+def plot_simulation_output(df_simulated_data):
     df_simulated_data = df_simulated_data[["S", "E", "I", "R", "E+I", "E+I+R"]]
 
     fig1 = plt.figure()
@@ -21,15 +21,18 @@ def plot_simulation_output(df_simulated_data, zoom_on=None, zoom_length=60):
     ax = sns.lineplot(data=df_simulated_data[["E", "I", "E+I", "E+I+R"]])
     ax.set_title("Apenas Expostos e Infectados")
 
-    if zoom_on is not None:
-        zoom_end = (pd.Timestamp(zoom_on) + pd.DateOffset(days=zoom_length)).date()
-        fig3 = plt.figure()
-        sns.despine()
-        plt.grid()
-        ax = sns.lineplot(
-            data=df_simulated_data[["E", "I", "E+I"]][zoom_on:zoom_end], markers=True
-        )
-        ax.set_title(f"Zoom (de {zoom_on} a {zoom_end})")
-        plt.xticks(fontsize=8, rotation=30)
+    zoom_length = 30
+    peak_date = df_simulated_data["I"].idxmax().date()
+    zoom_on = (pd.Timestamp(peak_date) - pd.DateOffset(days=zoom_length)).date()
+
+    zoom_end = (pd.Timestamp(peak_date) + pd.DateOffset(days=zoom_length)).date()
+    fig3 = plt.figure()
+    sns.despine()
+    plt.grid()
+    ax = sns.lineplot(
+        data=df_simulated_data[["E", "I", "E+I"]][zoom_on:zoom_end], markers=True
+    )
+    ax.set_title(f"Zoom (de {zoom_on} a {zoom_end})")
+    plt.xticks(fontsize=8, rotation=30)
 
     return st.pyplot(fig1), st.pyplot(fig2), st.pyplot(fig3)

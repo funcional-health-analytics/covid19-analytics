d82a89e06562c0619c2fe0f29fcfa4002e188d61
Gustavo Millen <millengustavo@gmail.com>
2020-04-19 14:59:36 -0300

    fix plot style and return

--- src/plot_simulation.py
+++ src/plot_simulation.py
@@ -3,25 +3,33 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 
+# plt.style.use("seaborn-whitegrid")
+
 
 def plot_simulation_output(df_simulated_data, zoom_on=None, zoom_length=60):
     df_simulated_data = df_simulated_data[["S", "E", "I", "R", "E+I", "E+I+R"]]
 
-    fig = plt.figure()
+    fig1 = plt.figure()
+    sns.despine()
+    plt.grid()
     ax = sns.lineplot(data=df_simulated_data)
     ax.set_title("Visão Geral da Epidemia")
-    plt.figure()
 
+    fig2 = plt.figure()
+    sns.despine()
+    plt.grid()
     ax = sns.lineplot(data=df_simulated_data[["E", "I", "E+I", "E+I+R"]])
     ax.set_title("Apenas Expostos e Infectados")
-    plt.figure()
 
     if zoom_on is not None:
         zoom_end = (pd.Timestamp(zoom_on) + pd.DateOffset(days=zoom_length)).date()
+        fig3 = plt.figure()
+        sns.despine()
+        plt.grid()
         ax = sns.lineplot(
             data=df_simulated_data[["E", "I", "E+I"]][zoom_on:zoom_end], markers=True
         )
         ax.set_title(f"Zoom (de {zoom_on} a {zoom_end})")
-        plt.figure()
+        plt.xticks(fontsize=8, rotation=30)
 
-    return st.pyplot(fig)
+    return st.pyplot(fig1), st.pyplot(fig2), st.pyplot(fig3)

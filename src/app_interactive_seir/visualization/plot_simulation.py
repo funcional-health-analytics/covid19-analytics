5651b9d99df7bc698b13d0f95676deed22b4bce8
Gustavo Millen <millengustavo@gmail.com>
2020-04-25 10:21:21 -0300

    adds markdown to plot and new display function

--- src/app_interactive_seir/visualization/plot_simulation.py
+++ src/app_interactive_seir/visualization/plot_simulation.py
@@ -6,23 +6,23 @@ import streamlit as st
 # plt.style.use("seaborn-whitegrid")
 
 
-def plot_simulation_output(df_simulated_data):
-    df_simulated_data = df_simulated_data[["S", "E", "I", "R", "E+I", "E+I+R"]]
+def plot_simulation_output(df: pd.DataFrame, target_location: str):
+    df = df[["S", "E", "I", "R", "E+I", "E+I+R"]]
 
     fig1 = plt.figure()
     sns.despine()
     plt.grid()
-    ax = sns.lineplot(data=df_simulated_data)
+    ax = sns.lineplot(data=df)
     ax.set_title("Visão Geral da Epidemia")
 
     fig2 = plt.figure()
     sns.despine()
     plt.grid()
-    ax = sns.lineplot(data=df_simulated_data[["E", "I", "E+I", "E+I+R"]])
+    ax = sns.lineplot(data=df[["E", "I", "E+I", "E+I+R"]])
     ax.set_title("Apenas Expostos e Infectados")
 
     zoom_length = 30
-    peak_date = df_simulated_data["I"].idxmax().date()
+    peak_date = df["I"].idxmax().date()
     zoom_on = (pd.Timestamp(peak_date) - pd.DateOffset(days=zoom_length)).date()
 
     zoom_end = (pd.Timestamp(peak_date) + pd.DateOffset(days=zoom_length)).date()
@@ -30,9 +30,21 @@ def plot_simulation_output(df_simulated_data):
     sns.despine()
     plt.grid()
     ax = sns.lineplot(
-        data=df_simulated_data[["E", "I", "E+I"]][zoom_on:zoom_end], markers=True
+        data=df[["E", "I", "E+I"]][zoom_on:zoom_end], markers=True
     )
     ax.set_title(f"Zoom (de {zoom_on} a {zoom_end})")
     plt.xticks(fontsize=8, rotation=30)
 
-    return st.pyplot(fig1), st.pyplot(fig2), st.pyplot(fig3)
+    return st.markdown(f"# Modelo SEIR - {target_location}"), st.pyplot(fig1), st.pyplot(fig2), st.pyplot(fig3)
+
+
+def display_peaks_from_simulation(peak_value: int, peak_date: pd.DatetimeIndex, category: str = "I") -> None:
+    category_dict = {
+        "S": "Suscetíveis",
+        "E": "Expostos",
+        "I": "Infectados",
+        "R": "Recuperados",
+    }
+    st.markdown(f"# Datas e Valores de Pico ({category_dict[category]})")
+    st.markdown(f"Data: **{str(peak_date)[:10]}**")
+    st.markdown(f"{category_dict[category]}: **{int(round(peak_value, 0))}**")

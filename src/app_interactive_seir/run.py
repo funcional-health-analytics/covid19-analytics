f56c4e81110213fd0729d51f795c2a81032e2109
Gustavo Millen <millengustavo@gmail.com>
2020-04-30 07:44:09 -0300

    removes redundant comments

--- src/app_interactive_seir/run.py
+++ src/app_interactive_seir/run.py
@@ -13,43 +13,31 @@ pd.set_option(
     "display.float_format", lambda x: "%.3f" % x
 )  # Granting that pandas won't use scientific notation for floating fields
 
-# Prints title and description
 print_description()
 
-# Loads epidemy data
 df_epidemy_data = load_epidemy_data(data_folder="data/")
 
-# Info about date
 print_date_info(df_epidemy_data)
 
-# Sidebar
 target_location = get_target_location(df_epidemy_data)
 
-# SEIR model parameters
 model_params = get_model_parameters()
 
-# Preprocess data
 df_data_target = prepare_data(df_epidemy_data, target_location)
 
-# Plot cases and deaths
 plot_cases_deaths(df_data_target, target_location)
 
-# Display last rows
 display_last_rows(df_data_target, nrows=5)
 
-# Display epidemy start numbers
 epidemy_start_numbers = get_epidemy_start_numbers(df_data_target, ncases=50)
 display_epidemy_start_numbers(epidemy_start_numbers, target_location)
 
-# Simulation execution
 simulate_trigger = st.sidebar.button("Simular!")
 if simulate_trigger:
     df_simulation_data = run_seir_model(model_params, epidemy_start_numbers)
 
-    # Plots from simulation
     plot_simulation_output(df_simulation_data, target_location)
 
-    # Max Points with date
     peak_value, peak_date = get_peaks_from_simulation(df_simulation_data, category="I")
     display_peaks_from_simulation(peak_value, peak_date, category="I")
 
